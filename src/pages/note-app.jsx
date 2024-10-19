f62c59efad3082d255555f2a8e47a8030cc3719d
RikkyM <rikky.mahendra54@gmail.com>
2024-10-19 18:40:31 +0700

    add Note-App

--- src/pages/note-app.jsx
+++ src/pages/note-app.jsx
@@ -1,21 +1,43 @@
 import Button from "../components/Elements/Button";
 
-const NoteAppPage = () => {
-    const tekan = (e) => {
-        e.preventDefault();
+const NoteAppPage = (props) => {
+  const tekan = (e) => {
+    e.preventDefault();
 
-        console.log('test');
-    }
+    e.target.judul.value && console.log(e.target.judul.value);
 
-    return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="max-w-xs w-full">
-                <form onSubmit={tekan}>
-                    <Button type="submit">Tekan</Button>
-                </form>
-            </div>
-        </div>
-    )
-}
+    const checkLocal = JSON.parse(localStorage.getItem('note-app')) || [];
+
+    localStorage.setItem(
+      "note-app",
+      JSON.stringify([...checkLocal, {
+        id: Date.now(),
+        judul: "test" + Date.now(),
+      }])
+    );
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-xs w-full">
+        <form onSubmit={tekan}>
+          <div className="mb-3">
+            <label htmlFor="judul" className="block mb-2 font-bold text-lg">
+              Judul
+            </label>
+            <input
+              className="px-3 py-2 text-sm border rounded-md w-full"
+              type="text"
+              name="judul"
+              id="judul"
+              placeholder="Masukkan Judul..."
+            />
+          </div>
+          <Button type="submit">Tekan</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default NoteAppPage;
